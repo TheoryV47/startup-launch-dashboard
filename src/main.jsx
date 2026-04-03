00ccb41292a71e1235f8b2894d67b6d451c25f52
TheoryV47 <theoryv47@gmail.com>
2026-04-03 16:53:17 +0400

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import StartupLaunchDashboard from './StartupLaunchDashboard'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <StartupLaunchDashboard />
+  </React.StrictMode>
+)
